ce66afc2d7af14392f2d612c4c374484ed341c13
omegamaster86 <masya868686@gmail.com>
2023-08-11 20:16:20 +0900

    add : member

--- src/app/components/aboutus.tsx
+++ src/app/components/aboutus.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import Member from './member'
 
-const aboutus = () => {
+const AboutUs: React.FC = () => {
   return (
-    <div>
-        
-    </div>
+    <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
+        <div className='container mx-auto px-11'>
+            <p className='leading-tight max-w-5xl mx-auto text-4xl tracking-tight'>
+                <strong>we will help you ship better app, faster</strong>
+                Our team of export engineers has created the best user experiences in some of the most popular apps worldwide.
+            </p>
+        </div>
+        <div className='container mx-auto px-11 text-center mt-28'>
+            <h2>Our Team</h2>
+            <div className='mt-2'>the &ldquo;spec-ops&rdquo;</div>
+            <div className='mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20'>
+                <Member id='marc' name='Marc' socialId='@mrousavy' link='https//github.com/mrousavy'/>
+                <Member id='szymon' name='Szymon' socialId='@szymon20000' link='https//github.com/szymon20000'/>
+                <Member id='thomas' name='Thomas' socialId='@thomas-coldwell' link='https//github.com/thomas-coldwell'/>
+                <Member id='marc' name='Marc' socialId='@mrousavy' link='https//github.com/mrousavy'/>
+                <Member id='szymon' name='Szymon' socialId='@szymon20000' link='https//github.com/szymon20000'/>
+                <Member id='thomas' name='Thomas' socialId='@thomas-coldwell' link='https//github.com/thomas-coldwell'/>
+            </div>
+        </div>
+    </section>
   )
 }
 
-export default aboutus
+export default AboutUs
