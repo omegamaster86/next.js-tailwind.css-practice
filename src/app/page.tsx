5f8a0e66b6c918ff640edaaaef8dd1ac6e69675a
omegamaster86 <masya868686@gmail.com>
2023-08-12 18:38:20 +0900

    add : skills components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutUs from './components/aboutus'
 import Masthead from './components/masthead'
 import ScrollObserver from './components/scroll-observer'
+import Skills from './components/skills'
 
 export default function Home() {
   return (
     <ScrollObserver>
         <Masthead />
         <AboutUs/>
+        <Skills/>
     </ScrollObserver>
 
   )
