9758822cd017063c010ca2104249e40e6b5ce283
omegamaster86 <masya868686@gmail.com>
2023-08-12 18:39:36 +0900

    add : imageLoaded useRef useContext

--- src/app/components/masthead.tsx
+++ src/app/components/masthead.tsx
@@ -1,14 +1,34 @@
+'use client'
 import React, { useRef, useContext, useState, useCallback  } from 'react'
 import Image from 'next/image'
+import { ScrollContext } from './scroll-observer'
 
 const Masthead: React.FC = () => {
+    const [imageLoaded, setImageLoaded] = useState(false)
+    const refContainer = useRef<HTMLDivElement>(null)
+    const { scrollY } = useContext(ScrollContext)
+    
+    let progress = 0
+
+    const { current: elContainer } = refContainer
+    if (elContainer) {
+        progress = Math.min(1, scrollY / elContainer.clientHeight)
+    }
+
+    const handleImageLoaded = useCallback(() => {
+        setImageLoaded(true)
+    },[])
+
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center bg-cyan-400'>
+    <div ref={refContainer} className='min-h-screen flex flex-col items-center justify-center bg-cyan-400 sticky top-0 -z-10'
+    style={{
+        transform: `translateY(-${progress * 20}vh)`
+    }}>
         <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover'>
             <source src='/assets/masthead-bg.m4v' type='video/mp4 codecs=hvc1'/>
             <source src='' type='video codecs=vp9'/>
         </video>
-        <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+        <div className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}>
             <Image src="assets/vercel.svg" width={128 / 3} height={114 / 3 } alt="logo"/>
         </div>
         <div className='p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col'>
@@ -17,8 +37,10 @@ const Masthead: React.FC = () => {
                 <span>App Development,</span> <span>done right.</span>
             </h2>
         </div>
-        <div className='flex-grow-0 pb-20 md:pb-10 transition-all duration-1000'>
-            <Image src="/assets/arrow-down.jpg" width={188 / 3} height={105 / 3} alt="scroll down" />
+        <div className={`flex-grow-0 pb-20 md:pb-10 transition-all duration-1000 ${
+            imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'
+        }`}>
+            <Image src="/assets/arrow-down.jpg" width={188 / 3} height={105 / 3} alt="scroll down" onLoad={handleImageLoaded}/>
         </div>
     </div>
   )
