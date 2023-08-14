677bf98398d83face2c74c3e32c7644f50749cd3
omegamaster86 <masya868686@gmail.com>
2023-08-14 12:04:33 +0900

    remove : indent

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -7,7 +7,6 @@ const opacityForBlock = (sectionProgress:number, blockNo:number) => {
     const progress = sectionProgress - blockNo
     if (progress >= 0 && progress < 1) return 1
     return 0.2
-
 }
 
 const Skills: React.FC = () => {
