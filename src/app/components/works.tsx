8d363ab3c2169e5a1d5940eae13af39453c0c0b4
omegamaster86 <masya868686@gmail.com>
2023-08-15 20:30:57 +0900

    add : TileContent

--- src/app/components/works.tsx
+++ src/app/components/works.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
-import TileWrapper from './tile'
+import {TileWrapper, TileBackground, TileContent, Tile } from './tile'
 
-const Works = () => <TileWrapper numOfPage={3} ></TileWrapper>
-
+const Works = () => (
+<TileWrapper numOfPages={3}>
+    <TileBackground></TileBackground>
+    <TileContent>
+        <Tile page={0} renderContent={({progress}) => (
+        <span>foo {progress}</span>)}></Tile>
+    </TileContent>
+    <TileContent>
+        <Tile page={1} renderContent={({progress}) => (
+        <span>foo1 {progress}</span>)}></Tile>
+    </TileContent>
+    <TileContent>
+        <Tile page={2} renderContent={({progress}) => (
+        <span>foo2 {progress}</span>)}></Tile>
+    </TileContent>
+</TileWrapper>
+)
 export default Works
