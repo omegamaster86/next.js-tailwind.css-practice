6c8d3363c79eae955fcb850b0b159a66e0b89361
omegamaster86 <masya868686@gmail.com>
2023-08-16 17:19:20 +0900

    add : WorkBackground

--- src/app/components/works.tsx
+++ src/app/components/works.tsx
@@ -1,21 +1,24 @@
-"use server";
+'use client'
 import React from 'react'
-import {TileWrapper, TileBackground, TileContent, Tile } from './tile'
+import {TileWrapper, TileBackground, TileContent,Tile } from './tile'
+import { WorkBackground }  from './work'
 
 const Works = () => (
 <TileWrapper numOfPages={3}>
-    <TileBackground></TileBackground>
+    <TileBackground>
+        <WorkBackground />
+    </TileBackground>
     <TileContent>
         <Tile page={0} renderContent={({progress}) => (
-        <span>foo {progress}</span>)}></Tile>
+        <span className='text-9xl'>foo {progress}</span>)}></Tile>
     </TileContent>
     <TileContent>
         <Tile page={1} renderContent={({progress}) => (
-        <span>foo1 {progress}</span>)}></Tile>
+        <span className='text-9xl'>foo1 {progress}</span>)}></Tile>
     </TileContent>
     <TileContent>
         <Tile page={2} renderContent={({progress}) => (
-        <span>foo2 {progress}</span>)}></Tile>
+        <span className='text-9xl'>foo2 {progress}</span>)}></Tile>
     </TileContent>
 </TileWrapper>
 )
