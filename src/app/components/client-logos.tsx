15d771321da1b6822e4f8b6cacb7cfc9cc7bc6eb
omegamaster86 <masya868686@gmail.com>
2023-08-17 20:09:23 +0900

    add : SliderItem component

--- src/app/components/client-logos.tsx
+++ src/app/components/client-logos.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Image from "next/image";
-import SliderContainer from "./slider"
+import SliderContainer, { SliderItem } from "./slider"
 
 const ClientLogos: React.FC = () => (
     <>
         <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+          <SliderItem width={150}>
             <Image src="/assets/vercel.svg" width={150} height={50} alt="vercel" objectFit="contain"/>
+          </SliderItem>
+          <SliderItem width={150}>
+            <Image src="/assets/vercel.svg" width={150} height={50} alt="vercel" objectFit="contain"/>
+          </SliderItem>
+          <SliderItem width={150}>
+            <Image src="/assets/vercel.svg" width={150} height={50} alt="vercel" objectFit="contain"/>
+          </SliderItem>
         </SliderContainer>
     </>
 )
