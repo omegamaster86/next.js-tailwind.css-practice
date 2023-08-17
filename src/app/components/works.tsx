f314d41e76f353853ebc7b02123b3ac03622f734
omegamaster86 <masya868686@gmail.com>
2023-08-17 15:47:19 +0900

    add : WorkLeft WorkRight Image

--- src/app/components/works.tsx
+++ src/app/components/works.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React from 'react'
 import {TileWrapper, TileBackground, TileContent,Tile } from './tile'
-import { WorkBackground }  from './work'
+import { WorkBackground, WorkContainer, WorkLeft, WorkLink, WorkRight, }  from './work'
+import Image from 'next/image'
 
 const Works = () => (
 <TileWrapper numOfPages={3}>
@@ -10,15 +11,47 @@ const Works = () => (
     </TileBackground>
     <TileContent>
         <Tile page={0} renderContent={({progress}) => (
-        <span className='text-9xl'>foo {progress}</span>)}></Tile>
+        <WorkContainer>
+            <WorkLeft progress={progress}>
+                <div>We build</div>
+                <div className='text-4xl md:text-5xl font-semibold tracking-tight'>Pink Panda&apos;s app
+                    <WorkLink href='https://bambooassets.com/'></WorkLink>
+                </div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+                <Image src="/assets/pinkpanda.jpg" layout="responsive" width={840} height={1620} alt="Pink Panda"/>
+            </WorkRight>
+        </WorkContainer>
+        )}
+        ></Tile>
     </TileContent>
     <TileContent>
         <Tile page={1} renderContent={({progress}) => (
-        <span className='text-9xl'>foo1 {progress}</span>)}></Tile>
+        <WorkContainer>
+            <WorkLeft progress={progress}>
+                <div>We helped</div>
+                <div className='text-4xl md:text-5xl font-semibold tracking-tight'>Showtime ship faster.</div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+                <Image src="/assets/showtime.jpg" layout="responsive" width={840} height={1620} alt="showtime"/>
+            </WorkRight>
+        </WorkContainer>
+        )}
+        ></Tile>
     </TileContent>
     <TileContent>
         <Tile page={2} renderContent={({progress}) => (
-        <span className='text-9xl'>foo2 {progress}</span>)}></Tile>
+        <WorkContainer>
+            <WorkLeft progress={progress}>
+                <div>Your image</div>
+                <div className='text-4xl md:text-5xl font-semibold tracking-tight'>image.iphone-bezels</div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+                <Image src="/assets/iphone-bezels.jpg" layout="responsive" width={840} height={1620} alt="iphone-bezels"/>
+            </WorkRight>
+        </WorkContainer>
+        )}
+        ></Tile>
     </TileContent>
 </TileWrapper>
 )
