2c2330fbc352500b6ee14cfa9f63d169f1877317
omegamaster86 <masya868686@gmail.com>
2023-08-19 18:40:02 +0900

    first commit

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Carousel from "./carousel";
+import CarouselItem from "./carsousel-item";
 
-const Testimonials: React.FC = () => <div>Testimonials</div>
+const Testimonials: React.FC = () => (
+<Carousel className="bg-black text-white py-10 lg:py-20 text-center">
+    <CarouselItem index={0}>
+        <div>test</div>
+    </CarouselItem>
+</Carousel>
+)
 
 export default Testimonials
