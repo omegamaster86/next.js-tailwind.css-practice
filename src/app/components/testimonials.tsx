c1d4236a90206ef29055d5d53c486cca265911e0
omegamaster86 <masya868686@gmail.com>
2023-08-20 19:22:36 +0900

    add : comment

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Carousel from "./carousel";
 import CarouselItem from "./carsousel-item";
+import Review from "./review";
 
 const Testimonials: React.FC = () => (
 <Carousel className="bg-black text-white py-10 lg:py-20 text-center">
     <CarouselItem index={0}>
-        <div>test</div>
+        <Review by="Axel (Showtime)">
+            Margelo and Showtime both share the love for high-quality software and the passion for building something people want.
+        </Review>
+    </CarouselItem>
+    <CarouselItem index={1}>
+        <Review by="Axel (Showtime)">
+            Margelo and Showtime both share the love for high-quality software and the passion for building something people want.
+        </Review>
+    </CarouselItem>
+    <CarouselItem index={2}>
+        <Review by="Axel (Showtime)">
+            Margelo and Showtime both share the love for high-quality software and the passion for building something people want.
+        </Review>
+    </CarouselItem>
+    <CarouselItem index={3}>
+        <Review by="Axel (Showtime)">
+            Margelo and Showtime both share the love for high-quality software and the passion for building something people want.
+        </Review>
+    </CarouselItem>
+    <CarouselItem index={4}>
+        <Review by="Axel (Showtime)">
+            Margelo and Showtime both share the love for high-quality software and the passion for building something people want.
+        </Review>
     </CarouselItem>
 </Carousel>
 )
