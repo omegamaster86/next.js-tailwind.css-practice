88280c2fafd5eae0701cc222981686e1ce96220c
omegamaster86 <masya868686@gmail.com>
2023-08-22 21:02:40 +0900

    change sitename

--- src/app/components/masthead.tsx
+++ src/app/components/masthead.tsx
@@ -32,7 +32,7 @@ const Masthead: React.FC = () => {
             <Image src="/assets/logo.png" width={128 / 3} height={114 / 3 } alt="logo"/>
         </div>
         <div className='p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col'>
-            <h1 className='mb-6 text-4xl xl:text-5xl'>Margelo</h1>
+            <h1 className='mb-6 text-4xl xl:text-5xl'>Demo site</h1>
             <h2 className='mb-2 text-2xl xl:text-3xl tracking-tight'>
                 <span>App Development,</span> <span>done right.</span>
             </h2>
