6adb5f6dcb588fd20a087fe553d52cb78ac51299
omegamaster86 <masya868686@gmail.com>
2023-08-26 22:29:11 +0900

    change : link→href

--- src/app/components/member.tsx
+++ src/app/components/member.tsx
@@ -6,15 +6,15 @@ interface props {
     id: string
     name: string
     socialId: string
-    link: string
+    href: string
 }
 
-const Member: React.FC<props> = ({ id, name, socialId, link }) => (
+const Member: React.FC<props> = ({ id, name, socialId, href }) => (
     <div>
         <Image src={`assets/margelo_faces_${id}.svg`} alt='name' width={1366} height={1555}/>
         <div className='text-2xl xl:text-3xl'>{name}</div>
         <div className='text-xl'>
-            <Link href={link}>
+            <Link href={href}>
                 {socialId}
             </Link>
         </div>
