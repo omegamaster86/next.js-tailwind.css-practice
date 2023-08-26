a5a2280e700cfc3712b49ad5d5860b24bcf89de2
omegamaster86 <masya868686@gmail.com>
2023-08-26 22:44:07 +0900

    target="_blank"

--- src/app/components/member.tsx
+++ src/app/components/member.tsx
@@ -7,14 +7,15 @@ interface props {
     name: string
     socialId: string
     href: string
+    target:string
 }
 
-const Member: React.FC<props> = ({ id, name, socialId, href }) => (
+const Member: React.FC<props> = ({ id, name, socialId, href, target }) => (
     <div>
         <Image src={`assets/margelo_faces_${id}.svg`} alt='name' width={1366} height={1555}/>
         <div className='text-2xl xl:text-3xl'>{name}</div>
         <div className='text-xl'>
-            <Link href={href}>
+            <Link href={href} target="_blank">
                 {socialId}
             </Link>
         </div>
